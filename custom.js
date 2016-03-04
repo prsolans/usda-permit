79286dc58e83abef426da34d511293ff66059299
Paul R. Solans <prsolans@gmail.com>
2016-03-04 09:48:27 -0600

    Accounted for URL params

--- custom.js
+++ custom.js
@@ -1,1 +1,21 @@
-console.log("PRS");
+function getParameterByName(name, url) {
+    if (!url) url = window.location.href;
+    name = name.replace(/[\[\]]/g, "\\$&");
+    var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+        results = regex.exec(url);
+    if (!results) return null;
+    if (!results[2]) return '';
+    return decodeURIComponent(results[2].replace(/\+/g, " "));
+}
+
+$(function () {
+	var firstName = getParameterByName("fname");
+	var lastName = getParameterByName("lname");
+	var email = getParameterByName("email");
+	var complianceNumber = getParameterByName("ca");
+
+	$("#00N36000006wnhQ").val(complianceNumber);
+	$("#first_name").val(firstName);
+	$("#last_name").val(lastName);
+	$("#email").val(email);
+});
